e3f55304f356e317741c773b059f462d4f4dfadf
veezyjay <viktorbasi545@gmail.com>
2020-03-24 21:28:52 +0100

    Add search bar and logs components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 import "./App.css";
 
 const App = () => {
@@ -8,7 +10,14 @@ const App = () => {
   useEffect(() => {
     M.AutoInit();
   });
-  return <div>My App</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
