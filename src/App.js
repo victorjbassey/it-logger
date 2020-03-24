cb81b68aad14b5eddeca764d677950c68f097117
veezyjay <viktorbasi545@gmail.com>
2020-03-24 20:55:28 +0100

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+
+const App = () => {
+  // Init materialize JS
+  useEffect(() => {
+    M.AutoInit();
+  });
+  return <div>My App</div>;
+};
+
+export default App;
